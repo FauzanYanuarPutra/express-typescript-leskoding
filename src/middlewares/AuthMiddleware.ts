ddb0082407d92856366e349cb42db10dd4c8dee6
fauzan <ccdplayerx@gmail.com>
2023-09-23 17:49:35 +0700

    permbaruan VIDEO 20/25

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -7,11 +7,16 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
   } 
 
   const secretKey = process.env.SECRET_KEY || "secret";
-  const token = req.headers.authorization.split(" ")[1];
+  const token: string = req.headers.authorization.split(" ")[1];
 
-  const credentials = jwt.verify(token, secretKey);
-  req.app.locals.credentials = credentials;
-  
+  try {
+    const credentials: string | object = jwt.verify(token, secretKey);
+    req.app.locals.credentials = credentials;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   next();
 } 
 
